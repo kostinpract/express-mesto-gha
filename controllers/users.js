e6c7adfaff187b6d43e61cbc6c24963965a785b3
Alexey Kostin <smartdesigner@gmail.com>
2023-05-16 13:42:23 +0300

    Arrow

--- controllers/users.js
+++ controllers/users.js
@@ -4,18 +4,18 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
-    .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getAllUsers = (req, res) => {
   User.find({})
-    .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
 };
