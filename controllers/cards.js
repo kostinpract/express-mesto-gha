87477c306e49d71c8001e7dee98b0898f2a86f5a
Alexey Kostin <smartdesigner@gmail.com>
2023-05-20 15:35:26 +0300

    feat: Add Like Methods

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,14 +12,14 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.getCard = (req, res) => {
-  Card.remove({ _id: req.params.cardId })
-    .then(() => res.send({}))
+  Card.findById({ _id: req.params.cardId })
+    .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findById({ _id: req.params.cardId })
-    .then((card) => res.send({ data: card }))
+  Card.remove({ _id: req.params.cardId })
+    .then(() => res.send({}))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 };
 
