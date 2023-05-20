c709929e93fb07bab91b63013911d74cf95bf54d
Alexey Kostin <smartdesigner@gmail.com>
2023-05-20 15:28:49 +0300

    feat: Add Like Methods

--- routes/cards.js
+++ routes/cards.js
@@ -10,7 +10,7 @@ const {
 
 router.post('/', createCard);
 router.get('/:cardId', getCard);
-router.delete('/:cardId', getCard);
+router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', changeLike);
 router.delete('/:cardId/likes', changeLike);
 router.get('/', getAllCards);
