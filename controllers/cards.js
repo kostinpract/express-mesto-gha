a98bbc7c32976cd904c48cabde52f250d3148d86
Alexey Kostin <alexey-kostin@yandex.ru>
2023-05-22 20:21:34 +0300

    Add Errors Handling

--- controllers/cards.js
+++ controllers/cards.js
@@ -45,8 +45,24 @@ module.exports.getCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove({ _id: req.params.cardId })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(ERROR_SERVER).send({ message: `Произошла ошибка ${err}` }));
+    .then((card) => {
+      if (!card) {
+        const error = new Error('Карточка с таким ID не найдена');
+        error.statusCode = ERROR_NOT_FOUND;
+        error.name = 'NotFound';
+        throw error;
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: `Указан некорректный ID карточки, ${err.name}` });
+      } else if (err.name === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: `Карточка с таким ID не найдена, ${err.name}` });
+      } else {
+        res.status(ERROR_SERVER).send({ message: `Произошла ошибка, ${err.name}` });
+      }
+    });
 };
 
 module.exports.getAllCards = (req, res) => {
@@ -85,6 +101,7 @@ const changeCard = (req, res, method) => {
 };
 
 module.exports.likeCard = (req, res) => changeCard(req, res, '$addToSet');
+
 module.exports.dislikeCard = (req, res) => changeCard(req, res, '$pull');
 
 // module.exports.changeLike = (req, res) => {
