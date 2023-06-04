aad3dc381e619e61b79c3b2f18f2aa1723ee3971
Alexey Kostin <smartdesigner@gmail.com>
2023-06-04 10:55:49 +0300

    pr14: joi

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ mongoose.connect(MONGOURI);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
   }),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
