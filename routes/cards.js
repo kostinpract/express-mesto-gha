95abc06c3f1871ba2c9ca068a0f7755fa334e03a
Alexey Kostin <alexey-kostin@yandex.ru>
2023-06-05 19:04:04 +0300

    pr14: autotest fixes

--- routes/cards.js
+++ routes/cards.js
@@ -39,7 +39,7 @@ router.post('/', celebrate({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
-}),, createCard);
+}), createCard);
 
 router.get('/', getAllCards);
 
