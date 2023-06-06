f5b4e26faa074c03a71b4b63ec7d84304cf1cbad
Alexey Kostin <smartdesigner@gmail.com>
2023-06-06 09:27:29 +0300

    pr14: review fix

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const { PORT = 3000 } = process.env;
 const { MONGOURI = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { SECRET = 'super-strong-secret' } = process.env;
 const REGEXPR = /https?:\/\/(www\.)?[-a-zA-Z0-9:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-const SECRET = 'super-strong-secret';
 
 module.exports = {
-  SECRET,
   PORT,
   MONGOURI,
+  SECRET,
   REGEXPR,
 };
